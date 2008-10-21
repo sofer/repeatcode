463e54205b3d4e8e9f27e7f90d4c8760e6e9c1f5
Danny <danny@Macintosh.local>
2008-10-21 19:51:17 +0100

    more changes

--- public/javascripts/lesson.js
+++ public/javascripts/lesson.js
@@ -100,7 +100,6 @@ RC.question = {
 		$("#current_interval").html(this.data.question.current_interval.toString());
 		$('#response-field').val(this.data.exercise.hint);
 		if (this.data.question.current_interval === 0) {
-		  $('#try').hide();
 		 	$('#expected').show();
 			$("#try-now").focus();
 		} else {
@@ -171,12 +170,12 @@ $(document).ready(function(){
 		var expected = $("#exercise-response").html().simplify();
 		var pattern = new RegExp(expected);
     var response = $("#response-field").val();
+		$('#try').hide();
 		if (pattern.test(response.simplify())) {
 			RC.current.post_response('correct');
-			wait_next();
       $('#correct').show().fadeOut(1000);
+			wait_next();
 		} else {
-			$('#try').hide();
 			$('#unexpected').show();
 			$(".wrong").html(response);
 			$("#try-again").focus();
@@ -199,51 +198,10 @@ $(document).ready(function(){
 
 	$("#show-answer").click(function() {
 		RC.current.post_response('incorrect');
-		RC.current.question.current_interval = 0;
+		RC.current.data.question.current_interval = 0;
 		$('#unexpected').hide();
 		$('#expected').show();
 		$("#try-now").focus();
-	});
-
-	$(".toggle").click(function(){
-		$(this).toggle();
-		return false;
-	});
-
-	// click inside a toggle-switch to toggle all toggleable elements
-	$(".toggle-switch a").click(function(){
-		$(".toggleable").toggle();
-		return false;
-	});
-
-	// click inside append-to-list to add list-item to list
-	$(".append-to-list a").click(function(){
-		$("#list-template").children().clone().appendTo("#list");
-		return false;
-	});
-	
-	//split phrase/response/hint/insert in textarea and populate #list from #list-template
-	$("#add-items").click(function(){
-		import_text = $("#import").val();
-		import_array = import_text.split("\n");
-		for (var i = 0; i < import_array.length; i += 1) {
-			item = $("#list-template").html()
-			terms = import_array[i].split("\t");
-			phrase = terms[0];
-			response = terms[1];
-			hint = terms[2];
-			insert = terms[3];
-			if (phrase != "" && response != "") {
-				item = $(item);
-				$(".phrase input", item).val(phrase);
-				$(".response input", item).val(response);
-				$(".hint input", item).val(hint);
-				$(".insert input", item).val(insert);
-				$("#list").append(item);
-			}
-		}
-		$(".toggleable").toggle();
-		return false;
 	});
 
 	$("#response-field").each(function(){
