0d3b5f6cde5630ef7d67e819e0be298088547c15
danny sofer <d@sofer.com>
2009-02-03 15:45:27 +0000

    add auto palette to lessons/show

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,48 +1,49 @@
 module LessonsHelper
 
   ENCODINGS = {
-  	'ß' => '&szlig',
-  	'à' => '&agrave',
-  	'á' => '&aacute',
-  	'â' => '&acirc',
-  	'ã' => '&atilde',
-  	'ä' => '&auml',
-  	'å' => '&aring',
-  	'æ' => '&aelig',
-  	'ç' => '&ccedil',
-  	'è' => '&egrave',
-  	'é' => '&eacute',
-  	'ê' => '&ecirc',
-  	'ë' => '&euml',
-  	'ì' => '&igrave',
-  	'í' => '&iacute',
-  	'î' => '&icirc',
-  	'ï' => '&iuml',
-  	'ð' => '&eth',
-  	'ñ' => '&ntilde',
-  	'ò' => '&ograve',
-  	'ó' => '&oacute',
-  	'ô' => '&ocirc',
-  	'õ' => '&otilde',
-  	'ö' => '&ouml',
-  	'÷' => '&divide',
-  	'ø' => '&oslash',
-  	'ù' => '&ugrave',
-  	'ú' => '&uacute',
-  	'û' => '&ucirc',
-  	'ü' => '&uuml',
-  	'ý' => '&yacute',
-  	'þ' => '&thorn'
+  	'ß' => '&szlig;',
+  	'à' => '&agrave;',
+  	'á' => '&aacute;',
+  	'â' => '&acirc;',
+  	'ã' => '&atilde;',
+  	'ä' => '&auml;',
+  	'å' => '&aring;',
+  	'æ' => '&aelig;',
+  	'ç' => '&ccedil;',
+  	'è' => '&egrave;',
+  	'é' => '&eacute;',
+  	'ê' => '&ecirc;',
+  	'ë' => '&euml;',
+  	'ì' => '&igrave;',
+  	'í' => '&iacute;',
+  	'î' => '&icirc;',
+  	'ï' => '&iuml;',
+  	'ð' => '&eth;',
+  	'ñ' => '&ntilde;',
+  	'ò' => '&ograve;',
+  	'ó' => '&oacute;',
+  	'ô' => '&ocirc;',
+  	'õ' => '&otilde;',
+  	'ö' => '&ouml;',
+  	'÷' => '&divide;',
+  	'ø' => '&oslash;',
+  	'ù' => '&ugrave;',
+  	'ú' => '&uacute;',
+  	'û' => '&ucirc;',
+  	'ü' => '&uuml;',
+  	'ý' => '&yacute;',
+  	'þ' => '&thorn;',
+  	'x' => '&euml;'
   }
 
-  def escape(char)
+  def HTML_encode(char)
     return ENCODINGS[char]
   end
   
   def palette(str)
     pal = ''
     str.each_char do |ch|
-      pal += ch + ', '
+      pal += "<div class=\"button\" value=\"#{ch}\">#{HTML_encode(ch)}</div>"
     end
     return pal
   end
